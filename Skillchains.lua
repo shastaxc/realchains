bde7e2ad4ad0ab762c608edb5c09408f08dd1239
shastaxc <shastaxc@gmail.com>
2025-04-29 00:08:39 -0400

    Update attribution

--- Skillchains.lua
+++ Skillchains.lua
@@ -1,5 +1,5 @@
 --[[
-Copyright © 2017, Ivaar
+Copyright © 2025, Silvermutt
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
@@ -10,14 +10,14 @@ modification, are permitted provided that the following conditions are met:
 * Redistributions in binary form must reproduce the above copyright
   notice, this list of conditions and the following disclaimer in the
   documentation and/or other materials provided with the distribution.
-* Neither the name of SkillChains nor the
+* Neither the name of realchains nor the
   names of its contributors may be used to endorse or promote products
   derived from this software without specific prior written permission.
 
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
 ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL IVAAR BE LIABLE FOR ANY
+DISCLAIMED. IN NO EVENT SHALL SILVERMUTT BE LIABLE FOR ANY
 DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
 (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
 LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
@@ -25,10 +25,10 @@ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ]]
-_addon.author = 'Ivaar'
-_addon.command = 'sc'
-_addon.name = 'SkillChains'
-_addon.version = '2.20.08.25'
+_addon.author = 'Silvermutt'
+_addon.command = 'real'
+_addon.name = 'realchains'
+_addon.version = '1.0.0'
 
 require('luau')
 require('pack')
